746fa379659ebccd2c4e77b4a95c7cca39965a84
mrthlinh <mrthlinh@gmail.com>
2017-02-11 13:40:05 -0600

    Oracle Xstream

---
+++ src/xstream/xstream_class_31.java
@@ -0,0 +1,36 @@
+package xstream;
+//http://www.studytrails.com/java/xml/xstream/xstream-xml-to-java/
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+
+public class xstream_class_31 {
+   static String xml1 = "<Music><type>rock</type></Music>";
+
+   public static void main(String[] args) {
+       XStream xStream = new XStream();
+       xStream.processAnnotations(Music.class);
+       Music musicObject = (Music) xStream.fromXML(xml1);
+       System.out.println(musicObject);
+       // prints Music [type=ROCK]
+   }
+
+}
+
+@XStreamAlias("Music")
+class Music {
+   public enum genre {
+       ROCK, JAZZ
+   };
+
+   genre type;
+
+   @Override
+   public String toString() {
+       return "Music [type=" + type + "]";
+   }
+
+}
+
+    
+   
---
+++ src/xstream/xstream_class_39.java
@@ -0,0 +1,34 @@
+package xstream;
+//ID = 26862650
+import java.io.Writer;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.core.util.QuickWriter;
+import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
+import com.thoughtworks.xstream.io.xml.PrettyPrintWriter;
+import com.thoughtworks.xstream.io.xml.XppDriver;
+
+public class xstream_class_39 {
+
+	private static XStream xstream = new XStream(new XppDriver() {
+	        @Override
+	        public HierarchicalStreamWriter createWriter(Writer out) {  
+	            return new PrettyPrintWriter(out) {  
+	                // Add CDATA block  
+	                boolean cdata;//I need to add cdata for some fields only  
+	                boolean isNeedUpper;//only some will need this
+
+	                protected void writeText(QuickWriter writer, String text) {  
+	                    if (cdata) {  
+	                        writer.write("<![CDATA[");  
+	                        writer.write(text);  
+	                        writer.write("]]>");  
+	                    } else if (isNeedUpper){  
+	                        writer.write(text.toUpperCase());  
+	                    }  
+	                    else writer.write(text);
+	                }  
+	            };  
+	        }  
+	    });
+}
